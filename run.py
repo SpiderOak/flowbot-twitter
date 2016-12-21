5a52fd9a9adf91b67a92bbbca265a0316fae64a7
Quentin Donnellan <quentin@spideroak.com>
2016-12-21 11:03:05 -0600

    Added setproctitle so that this twitterbot can distinguish itself from the other running bots in the command line

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from twitterbot.bot import TwitterBot
 import logging
 import json
+from setproctitle import setproctitle
 
 
 if __name__ == "__main__":
@@ -8,6 +9,9 @@ if __name__ == "__main__":
 
     with open('settings.json') as data_file:
         settings = json.load(data_file)
+
+    proctitle = 'TwitterBot ({0})'.format(settings.get('username'))
+    setproctitle(proctitle)
 
     bot = TwitterBot(settings)
     bot.run(block=False)
